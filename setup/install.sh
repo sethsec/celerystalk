2a0dacfce1c883c23fa9507d732d7d68dc572906
Seth Art <sethsec@gmail.com>
2020-04-27 23:06:04 -0400

    Reverting install changes, switching to ubuntu 18.04 for circleci.

--- setup/install.sh
+++ setup/install.sh
@@ -72,7 +72,7 @@ elif [ "$DISTRO" == "ubuntu" ]; then
         apt-get install wget docker.io python-pip python3-pip unzip redis-server chromium-bsu jq -y
     else
         apt-get update -y
-        apt-get install wget python-pip python3-pip unzip redis-server chromium-bsu jq -y
+        apt-get install wget curl vim python-pip python3-pip unzip redis-server chromium-bsu jq -y
 
     fi
 fi
